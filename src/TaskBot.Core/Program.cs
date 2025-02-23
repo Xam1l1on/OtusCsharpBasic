a9e4b0d545bb5b08828fd6814b0496302fd25eb5
k.solomatin <kir.solomatin@gmail.com>
2025-02-23 09:44:47 +0300

    Add project "TaskBot.Core"

---
+++ src/TaskBot.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskBot.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
